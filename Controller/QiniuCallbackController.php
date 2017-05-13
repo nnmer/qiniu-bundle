402dd287fafcb23b142cb5534feeffb9cd66945b
Anton Romanov <anton.v.romanov@gmail.com>
2017-05-14 02:09:25 +0800

    fix persistenceNotifyUrlAction callback data accepting

--- Controller/QiniuCallbackController.php
+++ Controller/QiniuCallbackController.php
@@ -34,7 +34,11 @@ class QiniuCallbackController extends Controller implements QiniuCallbackControl
      */
     public function persistenceNotifyUrlAction(Request $request)
     {
-        parse_str($request->getContent(),$payload);
+        $payload = json_decode($request->getContent(), true);
+        if (null === $payload){
+            throw new \LogicException('Payload received from Qiniu is not a json content');
+        }
+
         $this->get('event_dispatcher')->dispatch(QiniuEvents::PERSISTENCE_RESULTS_RECEIVED, new QiniuPersistenceEvent($payload));
 
         return new JsonResponse();
